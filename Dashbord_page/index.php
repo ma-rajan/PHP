1c437ff874e7c9cdb4f8aafa5040cc37d3675540
rajan <rajancdy14@gmail.com>
2026-06-30 21:52:48 +0545

    Dashbord page of serivce provider just learning flexbox and grid also

---
+++ Dashbord_page/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="nav1">
+         <a href="" id="logo"><img src="pictures/recycle.png" alt="images" ></a><h2> Home Service</h2>
+
+        </div>
+       
+        <div class="nav2">
+            <a href="">Home</a>
+            <a href="">Find Services</a>
+            <a href="">Login</a>
+            <button type="submit">Get Started</button>
+        </div>
+    </div>
+    <hr>
+
+    <div class="hero">
+
+
+        <div class="hero-1">
+        <h1>
+        Find Trusted Home <br>
+        Service Professionals<br>
+         Near You
+        </h1>
+        <p>Connect with verified, skilled professionals for all your home service needs</p>
+        <div class="search">
+            <div class="search1">
+             <input type="text" placeholder="search for services">
+               <button>search</button>  
+            </div>
+        </div>    
+</div>
+    </div>
+
+    <div class="icons">
+        <h3>Popular Services</h3>
+        <div class="services">
+            <div class="electrician">
+                <img src="pictures/plugin.png" alt="electrician"><p>Electrician</p>
+            </div>
+             <div class="airconditioner">
+                <img src="pictures/air-conditioner.png" alt="airconditioner"><p>Air conditioner</p>
+            </div>
+             <div class="carpenter">
+                <img src="pictures/carpenter.png" alt="carpenter"><p>Carpenter</p>
+            </div>
+             <div class="cleaner">
+                <img src="pictures/home.png" alt="cleaner"><p>Cleaner</p>
+            </div>
+             <div class="veteinary">
+                <img src="pictures/medical-report.png" alt="veteinary"><p>Veteinary</p>
+            </div>
+             <div class="plumber">
+                <img src="pictures/plumber.png" alt="plumber"><p>Plumber</p>
+            </div>
+             <div class="tutor">
+                <img src="pictures/tutoring.png" alt="tutoring"><p>Tutoring</p>
+            </div>
+             <div class="panter">
+                <img src="pictures/varnish.png" alt="panter"><p>Panter</p>
+            </div>
+        </div>
+            <h2>Featured Service Providers</h2>
+    </div>
+
+    <div class="service-card">
+        <div class="card1">
+            <img src="providers-img/1.jpg" alt="electrican">
+            <h4>Dogies baby</h4>
+            <p>Electrician</p>
+            <button>View-Profile</button>
+        </div>
+         <div class="card1">
+            <img src="providers-img/2.jpg" alt="plumber">
+            <h4> Dora</h4>
+            <p>Cleaner</p>
+            <button>View-Profile</button>
+        </div>
+         <div class="card1">
+            <img src="providers-img/3.jpg" alt="plumber">
+            <h4> Volt Vikram </h4>
+            <p>Plumber</p>
+            <button>View-Profile</button>
+        </div>
+    </div>
+
+
+    <div class="footer">
+            <p>© 2026 HomeService. All rights reserved.</p>
+            <div class="icons">
+            <a href="">
+                😀
+            </a>
+            <a href="">
+                😊
+            </a>
+
+            <a href="">
+                🟰
+            </a>
+
+            </div>
+    </div>
+    
+</body>
+</html>
